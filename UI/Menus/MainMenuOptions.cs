f6f29912f8744fea3b85772e8947e34855cd9789
Adam Golec <algolec1990@yahoo.com>
2025-02-04 20:21:55 -0500

    move MainMenuOptions enum into it's own .cs file within the UI/Menus directory.

---
+++ UI/Menus/MainMenuOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.UI.Menus
+{
+    public enum MainMenuOptions
+    {
+        SOLITARE = 1,
+        SLOTS = 2,
+        EXIT = 3
+    }
+}
