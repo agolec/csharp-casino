f21c23adca517b1f5e3c3df81a8719daeb33f4f9
Adam Golec <algolec1990@yahoo.com>
2025-02-06 11:05:59 -0500

    set up MenuHandler class to contain menus.

---
+++ UI/Handlers/MenuHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.UI.Handlers
+{
+    internal class MenuHandler
+    {
+        private Menu _mainMenu;
+
+        public MenuHandler()
+        {
+            InitializeMenus();
+        }
+        private void InitializeMenus()
+        {
+            _mainMenu = new Menu("Casino", "Play Solitare", "Play Slots");
+        }
+
+        public int GetMainMenuSelection()
+        {
+            return _mainMenu.GetUserSelection();
+        }
+
+    }
+}
