20026dc57d896b763f234acf7832b53e1c43a6d5
Adam Golec <algolec1990@yahoo.com>
2025-02-09 18:06:00 -0500

    Create enum for SlotsMenuOptions.

---
+++ UI/Menus/SlotsMenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.UI.Menus
+{
+    internal enum SlotsMenuOptions
+    {
+        PLACE_BET = 1,
+        VIEW_PAYOUT = 2,
+        CHECK_BALANCE = 3,
+        CHANGE_BET = 4,
+        AUTO_SPIN = 5,
+        EXIT_TO_MAIN_MENU = 6
+    }
+}
