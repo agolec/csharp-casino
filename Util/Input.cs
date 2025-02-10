0c1d482549572691f028da3324a7d3e0ba7e5bc8
Adam Golec <algolec1990@yahoo.com>
2025-02-09 22:32:48 -0500

    Set up number validation utility method in Util namespace.

---
+++ Util/Input.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Util
+{
+    internal class Input
+    {
+        public static int GetNumberFromInput(string prompt, string errorMessage)
+        {
+            int number;
+            do
+            {
+                Console.Write(prompt);
+                if(int.TryParse(Console.ReadLine(), out number))
+                {
+                    return number;
+                }
+                Console.WriteLine(errorMessage);
+            } while (true);
+        }
+    }
+}
