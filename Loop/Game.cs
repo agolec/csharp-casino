ee75cb759af43a9d4feb1ade6f9595552a56e1ad
Adam Golec <algolec1990@yahoo.com>
2025-02-10 11:21:38 -0500

    remove DisplaySlotMenu method and move logic to RunSlotsMenu inside MenuHandler class. Also remove StatsMenu related code and place it likewise in MenuHandler class.

--- Loop/Game.cs
+++ Loop/Game.cs
@@ -33,7 +33,6 @@ namespace Casino.Loop
         public void PlaySlots()
         {
             _menuHandler.RunSlotsMenu(this);
-            DisplaySlotMenu();
         }
 
         public void PullLever()
@@ -45,52 +44,6 @@ namespace Casino.Loop
                 slots.SpinReels();
             }
 
-        }
-        private void DisplaySlotMenu()
-        {
-            bool running = true;
-            do
-            {
-                switch (_menuHandler.GetSlotsMenuSelection())
-                {
-                        case (int)SlotsMenuOptions.PLACE_BET:
-                            Console.WriteLine("Going to place bet;");
-                            PullLever();
-                            break;
-                        case (int)SlotsMenuOptions.CHECK_STATS:
-                            Console.WriteLine("Checking stats");
-                            DisplayStatsMenu();
-                            break;
-                        case (int)SlotsMenuOptions.VIEW_PAYOUT:
-                            Console.WriteLine("Viewing payout...");
-                            break;
-                        case (int)SlotsMenuOptions.CHANGE_BET:
-                            Console.WriteLine("Changing Bet");
-                            break;
-                        case (int)SlotsMenuOptions.AUTO_SPIN:
-                            Console.WriteLine("Auto spin");
-                            break;
-                        case (int)SlotsMenuOptions.EXIT_TO_MAIN_MENU:
-                            running = false;
-                            break;
-                }
-            } while (running);
-        }
-        private void DisplayStatsMenu()
-        {
-            bool displayMenu = true;
-            do
-            {
-                switch (_menuHandler.GetStatsMenuSelection())
-                {
-                    case (int)StatsMenuOptions.RETURN_TO_GAME:
-                        displayMenu = false;
-                        break;
-                    default:
-                        Console.WriteLine("Error: invalid menu input.");
-                        break;
-                }
-            } while (displayMenu);
         }
     }
 }
