8c380c2a15ce8155a418eb6b9c80611e4a951650
Adam Golec <algolec1990@yahoo.com>
2025-02-10 11:19:18 -0500

    create methods RunSlotsMenu, RunMainMenu, and HandleStasMenu for displaying menus.

--- UI/Handlers/MenuHandler.cs
+++ UI/Handlers/MenuHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Casino.Loop;
+using Casino.UI.Menus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,5 +38,64 @@ namespace Casino.UI.Handlers
             return _statsMenu.GetUserSelection();
         }
 
+        public bool RunMainMenu(Game game)
+        {
+            switch (this.GetMainMenuSelection())
+            {
+                case (int)MainMenuOptions.SOLITARE:
+                    Console.WriteLine("ToDo: make Solitare game.");
+                    return true;
+                case (int)MainMenuOptions.SLOTS:
+                    Console.WriteLine("going to play slots.");
+                    game.PlaySlots();
+                    return true;
+                case (int)MainMenuOptions.EXIT:
+                    Console.WriteLine("Exiting program.");
+                    return false;
+                default:
+                    Console.WriteLine("Invalid selection.");
+                    return true;
+            }
+        }
+        public void RunSlotsMenu(Game game)
+        {
+            bool running = true;
+            while (running)
+            {
+                switch (this.GetSlotsMenuSelection())
+                {
+                    case (int)SlotsMenuOptions.PLACE_BET:
+                        Console.WriteLine("Going to place bet;");
+                        game.PullLever();
+                        break;
+                    case (int)SlotsMenuOptions.CHECK_STATS:
+                        Console.WriteLine("Checking stats");
+                        game.DisplayStatsMenu();
+                        break;
+                    case (int)SlotsMenuOptions.VIEW_PAYOUT:
+                        Console.WriteLine("Viewing payout...");
+                        break;
+                    case (int)SlotsMenuOptions.CHANGE_BET:
+                        Console.WriteLine("Changing Bet");
+                        break;
+                    case (int)SlotsMenuOptions.AUTO_SPIN:
+                        Console.WriteLine("Auto spin");
+                        break;
+                    case (int)SlotsMenuOptions.EXIT_TO_MAIN_MENU:
+                        running = false;
+                        break;
+                }
+            }
+        }
+        public void HandleStatsMenu()
+        {
+            while (true)
+            {
+                if(_statsMenu.GetUserSelection() == (int)StatsMenuOptions.RETURN_TO_GAME)
+                {
+                    break;
+                }
+            }
+        }
     }
 }
